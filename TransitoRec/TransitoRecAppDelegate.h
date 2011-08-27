f9c85f94ab2ccf99eeb41d0cc1d4b6bff9dd7390
luiztiago <luiztiago@gmail.com>
2011-08-27 13:31:47 -0300

    Initial Commit

---
+++ TransitoRec/TransitoRecAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TransitoRecAppDelegate.h
+//  TransitoRec
+//
+//  Created by Luiz Tiago Alves de Oliveira on 27/08/11.
+//  Copyright 2011 iR4 Consultoria Web. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransitoRecAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
